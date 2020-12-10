0547e3ca9062b57ac729eb0b23c983cc8574ead0
Victoramores <victorjesus.amores@upm.es>
2020-12-10 16:43:12 +0100

    Adding Some crashing bugs from CSV

--- src/Utils.jl
+++ src/Utils.jl
@@ -1,8 +1,9 @@
 # Adding Some useful functions
 import CSV
+import DataFrames
 
 function readquadrature(filename::String, delimiter)
-    quadFile = CSV.read(joinpath(@__DIR__,"quadDir", filename), delim = delimiter)
+    quadFile = CSV.read(joinpath(@__DIR__,"quadDir",filename) , DataFrame, delim = delimiter)
     return quadFile.r1, quadFile.r2,
            quadFile.r3, quadFile.w
 end
